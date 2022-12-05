099b9b8daf7946572f7f9ed0b5727bc1f3186356
avinash uttamchandani <avinash@nonholonomy.com>
2022-12-05 16:22:14 -0500

    Some better assembly test files.

--- labs/03_rv32i_part2/asm/functions.s
+++ labs/03_rv32i_part2/asm/functions.s
@@ -1,6 +1,7 @@
 # Testing if our core can execute the jumps, loads, and stores for complex
 # function calls.
-# Note - ret = jr ra = jalr x0, ra, 0.
+# Note - call = jal ra, LABEL.
+# Note - ret =  jr ra = jalr x0, ra, 0.
 
 # Use our data memory bank for our stack pointer. 
 # Note - need to update this based on the size of DATA_MEM.
@@ -159,5 +160,39 @@ FIBONACCI:
   FIB_STACK_OVERFLOW:
     addi a0, zero, -1
     j FIB_STACK_POP_AND_RETURN
-
-             
+# |---------------------------------------|
+# | Final Register File State             |
+# |---------------------------------------|
+# |    x00, zero = 0x00000000 (         0)|
+# |      x01, ra = 0x000000cc (       204)|
+# |      x02, sp = 0x300003fc ( 805307388)|
+# |      x03, gp = 0xxxxxxxxx (         x)|
+# |      x04, tp = 0xxxxxxxxx (         x)|
+# |      x05, t0 = 0x0000000d (        13)|
+# |      x06, t1 = 0x00000011 (        17)|
+# |      x07, t2 = 0xxxxxxxxx (         x)|
+# |      x08, s0 = 0xxxxxxxxx (         x)|
+# |      x09, s1 = 0xxxxxxxxx (         x)|
+# |      x10, a0 = 0x0000000d (        13)|
+# |      x11, a1 = 0x00000064 (       100)|
+# |      x12, a2 = 0x00000002 (         2)|
+# |      x13, a3 = 0x00000001 (         1)|
+# |      x14, a4 = 0xxxxxxxxx (         x)|
+# |      x15, a5 = 0xxxxxxxxx (         x)|
+# |      x16, a6 = 0xxxxxxxxx (         x)|
+# |      x17, a7 = 0xxxxxxxxx (         x)|
+# |      x18, s2 = 0xxxxxxxxx (         x)|
+# |      x19, s3 = 0xxxxxxxxx (         x)|
+# |      x20, s4 = 0xxxxxxxxx (         x)|
+# |      x21, s5 = 0xxxxxxxxx (         x)|
+# |      x22, s6 = 0xxxxxxxxx (         x)|
+# |      x23, s7 = 0xxxxxxxxx (         x)|
+# |      x24, s8 = 0xxxxxxxxx (         x)|
+# |      x25, s9 = 0xxxxxxxxx (         x)|
+# |     x26, s10 = 0xxxxxxxxx (         x)|
+# |     x27, s11 = 0x00000019 (        25)|
+# |      x28, t3 = 0xxxxxxxxx (         x)|
+# |      x29, t4 = 0xxxxxxxxx (         x)|
+# |      x30, t5 = 0xxxxxxxxx (         x)|
+# |      x31, t6 = 0xxxxxxxxx (         x)|
+# |---------------------------------------|
--- labs/03_rv32i_part2/asm/irtypes.s
+++ labs/03_rv32i_part2/asm/irtypes.s
@@ -1,10 +1,57 @@
-add x1, x0, x0
-sub s2, s3, s4
-sll s5, s6, s9
-slt x1, x1, x1
-sltu x1, x2, x3
-xor t0, t1, t2
-srl ra, sp, gp
-sra tp, s0, fp
-or s1, a0, a1
-and a2, a3, a4
+addi  x1, x0, -1
+addi  x2, x0, 35
+xori  x3, x2, 68
+ori   x4, x2, 17
+andi  x5, x2, 17
+slli  x6, x1, 8
+srli  x7, x3, 2
+srai  x8, x6, 6
+slti  x9, x1, 35
+sltiu x10, x1, 35
+add   x11, x1, x1
+sub   x12, x1, x1
+xor   x13, x1, x12
+or    x14, x2, x3
+and   x15, x2, x3
+addi  x16, x0, 4
+sll   x17, x2, x16
+srl   x18, x3, x16
+sra   x19, x6, x16
+slt   x20, x1, x2
+sltu  x21, x1, x2
+# |---------------------------------------|
+# | Final Register File State             |
+# |---------------------------------------|
+# |    x00, zero = 0x00000000 (         0)|
+# |      x01, ra = 0xffffffff (        -1)|
+# |      x02, sp = 0x00000023 (        35)|
+# |      x03, gp = 0x00000067 (       103)|
+# |      x04, tp = 0x00000033 (        51)|
+# |      x05, t0 = 0x00000001 (         1)|
+# |      x06, t1 = 0xffffff00 (      -256)|
+# |      x07, t2 = 0x00000019 (        25)|
+# |      x08, s0 = 0xfffffffc (        -4)|
+# |      x09, s1 = 0x00000001 (         1)|
+# |      x10, a0 = 0x00000000 (         0)|
+# |      x11, a1 = 0xfffffffe (        -2)|
+# |      x12, a2 = 0x00000000 (         0)|
+# |      x13, a3 = 0xffffffff (        -1)|
+# |      x14, a4 = 0x00000067 (       103)|
+# |      x15, a5 = 0x00000023 (        35)|
+# |      x16, a6 = 0x00000004 (         4)|
+# |      x17, a7 = 0x00000230 (       560)|
+# |      x18, s2 = 0x00000006 (         6)|
+# |      x19, s3 = 0x0ffffff0 ( 268435440)|
+# |      x20, s4 = 0x00000001 (         1)|
+# |      x21, s5 = 0x00000000 (         0)|
+# |      x22, s6 = 0xxxxxxxxx (         x)|
+# |      x23, s7 = 0xxxxxxxxx (         x)|
+# |      x24, s8 = 0xxxxxxxxx (         x)|
+# |      x25, s9 = 0xxxxxxxxx (         x)|
+# |     x26, s10 = 0xxxxxxxxx (         x)|
+# |     x27, s11 = 0xxxxxxxxx (         x)|
+# |      x28, t3 = 0xxxxxxxxx (         x)|
+# |      x29, t4 = 0xxxxxxxxx (         x)|
+# |      x30, t5 = 0xxxxxxxxx (         x)|
+# |      x31, t6 = 0xxxxxxxxx (         x)|
+# |---------------------------------------|
